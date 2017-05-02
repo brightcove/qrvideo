a11604fe582748c9965cb3faf2a1c5a958266795
Gary Katsevman <git@gkatsev.com>
2017-05-02 11:09:00 -0400

    capture at 0.25 of a second and use Q EC for generation

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
-var kxing = require('kxing');
-window.kxing = kxing;
+var qrcode = require('qrstuv');
 
 var canvas = document.createElement('canvas');
+canvas.id = 'qr-canvas';
 var ctx;
 var video = document.querySelector('video');
 
@@ -20,36 +20,52 @@ function init() {
   document.body.appendChild(canvas);
   ctx = canvas.getContext('2d');
   window.ctx = ctx;
-
-  video.addEventListener('canplay', function testing() {
-    video.play();
-    testHarness();
-    video.removeEventListener('canplay', testing);
-  });
 };
 
-let i = 0;
+let i = -1;
+let timeout;
 function testHarness() {
-  const result = test();
+  i++;
+  ctx.drawImage(video, 0, 0);
+  var newDiv = document.createElement('div');
+  var canvas = document.createElement('canvas');
+  newDiv.appendChild(document.createTextNode(i));
+  newDiv.appendChild(canvas);
+  canvas.getContext('2d').drawImage(video, 0, 0);
+  document.body.appendChild(newDiv);
+    qrcode.decode().then(function(result) {
+      const ct = video.currentTime;
+      const to = 1000 - ((ct - Math.floor(ct)) - 0.25) * 1000;
+      const eq = Number(result) === Math.floor(ct);
+      if (eq) {
+        console.log("EQUAL", result, i, ct.toFixed(2), to.toFixed(2));
+      } else {
+        console.error("NOT EQUAL", result, i, ct.toFixed(2), to.toFixed(2));
+      }
 
-  console.log(result.text);
+      if (!video.ended) {
+        timeout = setTimeout(testHarness, to);
+      }
+    }).catch(function(e) {
+      const ct = video.currentTime;
+      const to = 1000 - ((ct - Math.floor(ct)) - 0.25) * 1000;
+      console.error(e, ct.toFixed(2), video.currentTime.toFixed(2), to.toFixed(2));
 
-  if (!video.ended) {
-    setTimeout(testHarness, 1000);
-  }
+    });
 }
 
 function test() {
   ctx.drawImage(video, 0, 0);
-  let result;
-  try {
-    result = kxing.KXing.getReader().decode(ctx.getImageData(0, 0, canvas.width, canvas.height));
-  } catch (e) {
-    console.log(e);
-    result = {text: null};
-  }
-
-  return result;
-};
+  return qrcode.decode();
+}
 
 window.test = test;
+window.start = function() {
+  video.play();
+  setTimeout(testHarness, 250);
+};
+window.qrcode = qrcode;
+window.stop = () => {
+  clearTimeout(timeout);
+  video.pause();
+}
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var path = require('path');
 
 for (var i = 0; i < 100; i++) {
   qr.image(i, {
+    ec_level: 'Q',
     size: 6
   }).pipe(fs.createWriteStream(path.join('qrs', 'img' + i + '.png')));
 }
