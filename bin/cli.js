c09e8046f40849c69f557dca81d341222cbad8f5
Gary Katsevman <git@gkatsev.com>
2017-05-02 16:49:30 -0400

    add cli options

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,36 @@
 #!/usr/bin/env node
 
 var qrvideo = require('./index.js');
+var args = require('minimist')(process.argv.slice(2), {
+  string: ['name', 'ec-level'],
+  boolean: ['keep-qrs'],
+  alias: {
+    n: 'name',
+    l: 'length',
+    e: 'ec-level',
+    k: 'keep-qrs',
+    f: 'framerate',
+    q: 'qrs-per-period',
+    s: 'size'
+  }
+});
 
-qrvideo.generateQrs({})
+console.log(args);
+
+qrvideo.generateQrs({
+  framerate: args.framerate,
+  qrsPerFramePeriod: args.q,
+  length: args.length,
+  size: args.size,
+  ec_level: args.e
+})
 .then(function() {
-  return qrvideo.generateVideo({name: 'foo.mp4'});
+  return qrvideo.generateVideo({
+    name: args.name,
+    framerate: args.framerate,
+    qrsPerFramePeriod: args.q,
+    length: args.length
+  });
 })
 .then(qrvideo.cleanupQrs, qrvideo.cleanupQrs)
 .then(function() {
